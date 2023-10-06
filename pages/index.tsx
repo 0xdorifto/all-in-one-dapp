7c87d74519e46934e2133143299c0cba3a18ae83
0xdorifto <gastaosousafaria@gmail.com>
2023-10-06 12:27:38 +0100

    Add personal description

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import { RiCameraLensFill } from "react-icons/ri";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="px-16">
       <Head>
         <title>0xdorifto</title>
         <meta content="Created by 0xdorifto" name="description" />
@@ -19,23 +19,45 @@ const Home: NextPage = () => {
         <ConnectButton />
       </header>
 
-      <main className="flex justify-around">
-        <div>
-          <h1>0xdorifto</h1>
-          <h2>Web3/Blockchain Developer</h2>
+      <main>
+        <div className="flex justify-around">
+          <div className="place-content-center">
+            <h1>0xdorifto</h1>
+            <h2>Web3/Blockchain Developer</h2>
+          </div>
+
+          <Image
+            className="rounded-full"
+            src="/takumi.jpg"
+            width={250}
+            height={250}
+            alt="Takumi"
+          />
         </div>
 
-        <Image
-          className="rounded-full"
-          src="/takumi.jpg"
-          width={250}
-          height={250}
-          alt="Takumi"
-        />
+        <div className="mt-16">
+          <h3>About myself:</h3>
+          <p>I&apos;m a 24 year old Software Engineer from Lisbon, Portugal</p>
+          <p>
+            In my second year of university, I stumbled across bitcoin which
+            made me interested so interested in blockchain I changed my major to
+            computer science so I could eventually work with this technology.
+          </p>
+          <p>
+            I&apos;ve been working full time in web3 for more than a year, in
+            which I participated in some hackathons, and doing front-end
+            development for over 2 years.
+          </p>
+          <p>
+            Right now, I&apos;m particularly interested in NFTs and their use
+            cases, such as gatekeeping, digital identification, tokenization of
+            real world assets, etc...
+          </p>
+        </div>
       </main>
 
-      <footer>
-        <p className="flex justify-center my-2">Made with 🔥 in 🇵🇹</p>
+      <footer className="border-t absolute bottom-0 inset-x-0 p-4">
+        <p className="flex justify-center mb-2">Made with 🔥 in 🇵🇹</p>
 
         {/* change color on hover */}
         <div className="flex justify-center">
